c66443197213d5dec07b0ae59284f8e0ca0163bd
Aaron Hardy <aahardy@adobe.com>
2019-03-04 14:36:03 -0700

    Show toast action complete.

--- src/lib/actions/showToast.js
+++ src/lib/actions/showToast.js
@@ -1,5 +1,11 @@
 'use strict';
+var loadScript = require('@adobe/reactor-load-script');
+// Loads tiny-toast: https://github.com/bahmutov/tiny-toast
+var loadScriptPromise = loadScript('https://bit.ly/2TvaI5o');
 
 module.exports = function(settings) {
-  // TODO Perform some action.
+  loadScriptPromise.then(function() {
+    var extensionSettings = turbine.getExtensionSettings();
+    tinyToast.show(settings.message).hide(extensionSettings.duration);
+  });
 };
